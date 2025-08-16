74d23b3a2ccb0393d018854b2b1ba5621e90055a
nagarajanvijay69 <nagarajanvijay69@gmail.com>
2025-08-16 17:05:12 +0530

    save

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { flushSync } from 'react-dom'
 import Navbar from '../Navbar/Navbar'
 import Sidebar from '../Sidebar/Sidebar'
 import './Chat.css'
@@ -7,7 +6,10 @@ import axios from 'axios'
 import { adduserPrompts, addresPrompts } from '../Redux/Slice'
 import { useSelector, useDispatch } from 'react-redux'
 import { Context } from './Context'
-
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import rehypeHighlight from 'rehype-highlight'
+// import "highlight.js/styles/";
 
 
 const Chat = () => {
@@ -21,25 +23,29 @@ const Chat = () => {
   const showres = resPrompts.length > 0 ? resPrompts[resPrompts.length - 1] : '';
 
 
-  var PerfectRes = showres.split('**').map((item, index) => {
-    const innerParts = item.split('*').map((subItem, subIndex) => {
-        if (subIndex % 2 === 0) {
-            return <span key={`sub-${index}-${subIndex}`}>{subItem}</span>;
-        } else {
-            return <span key={`sub-${index}-${subIndex}`}>{subItem} <br /></span>;
-        }
-    });
+  var PerfectRes = <ReactMarkdown
+   rehypePlugins={[rehypeHighlight]}
+   remarkPlugins={[remarkGfm]}
+   >{showres}</ReactMarkdown>;
+  // .split('**').map((item, index) => {
+  //     const innerParts = item.split('*').map((subItem, subIndex) => {
+  //         if (subIndex % 2 === 0) {
+  //             return <span key={`sub-${index}-${subIndex}`}>{subItem}</span>;
+  //         } else {
+  //             return <span key={`sub-${index}-${subIndex}`}>{subItem} <br /></span>;
+  //         }
+  //     });
 
-    if (index % 2 === 0) {
-        return <span key={`main-${index}`}>{innerParts}</span>;
-    } else {
-        return (
-            <span key={`main-${index}`}>
-                <br /><b>{innerParts}</b><br />
-            </span>
-        );
-    }
-});
+  //     if (index % 2 === 0) {
+  //         return <span key={`main-${index}`}>{innerParts}</span>;
+  //     } else {
+  //         return (
+  //             <span key={`main-${index}`}>
+  //                 <br /><b>{innerParts}</b><br />
+  //             </span>
+  //         );
+  //     }
+  // });
 
 
 
@@ -52,7 +58,7 @@ const Chat = () => {
     const tempvalue = Value;
     dispatch(adduserPrompts(tempvalue));
     setValue('');
-    
+
 
     try {
       const response = await axios.post('https://itachi-idb9.onrender.com/message', {
@@ -64,16 +70,16 @@ const Chat = () => {
       });
 
       const tempresponse = response.data.response;
-    
+
       dispatch(addresPrompts(tempresponse));
 
 
     } catch (error) {
       console.error("Error sending request:", error);
     }
-    
 
-   
+
+
     setLoading(false);
 
   }
@@ -82,7 +88,7 @@ const Chat = () => {
     <div className='root'>
       <Navbar />
       <div className="chat">
-        < Context.Provider value={{sendrequest}}>
+        < Context.Provider value={{ sendrequest }}>
           <Sidebar />
         </Context.Provider>
         <div className='chat-container'>
@@ -94,26 +100,26 @@ const Chat = () => {
               </div>
               <div className="chat-ai" >
                 <img src="./itachi.jpg" />
-                { loading ?
-                        <div className="text-res loading">
-                          <span>L</span>
-                          <span>o</span>
-                          <span>a</span>
-                          <span>d</span>
-                          <span>i</span>
-                          <span>n</span>
-                          <span>g</span>
-                          <span>.</span>
-                          <span>.</span>
-                          <span>.</span>
-                        </div>
-                  
-                : <div>
-                     <div className="text-res">{
-                      <span style={{animationDelay : '0.3s'}}>{PerfectRes}</span>
-                        
+                {loading ?
+                  <div className="text-res loading">
+                    <span>L</span>
+                    <span>o</span>
+                    <span>a</span>
+                    <span>d</span>
+                    <span>i</span>
+                    <span>n</span>
+                    <span>g</span>
+                    <span>.</span>
+                    <span>.</span>
+                    <span>.</span>
+                  </div>
+
+                  : <div>
+                    <div className="text-res markdown">{
+                      <span style={{ animationDelay: '0.3s' }}>{PerfectRes}</span>
+
                     }</div>
-                </div>
+                  </div>
                 }
               </div>
             </div>
@@ -123,12 +129,12 @@ const Chat = () => {
             <div className={Bool ? "hide-data" : "title-3 title"}>How can I help you ?</div>
           </div>
           <div className="two">
-            <input type="text" placeholder='Enter your prompt....' onChange={(e) => setValue(e.target.value)} value={Value} 
-            onKeyDown={(e)=>{
-              if (e.key === 'Enter') {
-                sendrequest(Value);
-              }
-            }} />
+            <input type="text" placeholder='Enter your prompt....' onChange={(e) => setValue(e.target.value)} value={Value}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  sendrequest(Value);
+                }
+              }} />
             <img src="send.svg" onClick={() => sendrequest(Value)} />
           </div>
 
